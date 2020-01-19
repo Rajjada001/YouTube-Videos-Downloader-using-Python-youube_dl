75eb4aeaa2c4b27409bcb05ecd79794fe1182446
Raj Jada <56466485+Rajjada001@users.noreply.github.com>
2020-01-19 18:46:01 +0530

    Update YoutubeDownload.py

--- YoutubeDownload.py
+++ YoutubeDownload.py
@@ -1,11 +1,7 @@
 from __future__ import unicode_literals
 import youtube_dl
+ydl_opts = {'format':' best[height<=?1080]'}
 url = input("Enter your URL:")
-
-ydl = youtube_dl.YoutubeDL({
-    'outtmpl': '/tmp/testvideo.mp4',
-    'format':' bestvideo+bestaudio'
-})
-
-ydl.download([url])
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+	ydl.download([url])
 print("Downloaded!")
