e3a87e8496c8b33795ee53daf650d32597e766b1
Raj Jada <56466485+Rajjada001@users.noreply.github.com>
2020-02-08 18:24:18 +0530

    Update YoutubeDownload.py

--- YoutubeDownload.py
+++ YoutubeDownload.py
@@ -1,7 +1,19 @@
 from __future__ import unicode_literals
 import youtube_dl
-ydl_opts = {'format':' (bestvideo[width>=?1920]/bestvideo)+bestaudio/best'}
-url = input("Enter your URL:")
+ydl_opts = {
+'outtmpl': '/Folder Name/%(title)s'+'.mp4',
+'format':' (bestvideo[width>=?1080]/bestvideo)+bestaudio/best',
+'allsubtitles': True,
+    'writesubtitles': True,
+    'convertsubtitles':True,
+   
+    'subtitleslangs':'en',
+    'postprocessors': [{
+        'key': 'FFmpegSubtitlesConvertor',
+        'format': 'srt',
+    }],
+}
+url = input("Enter your URL: ")
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 	ydl.download([url])
 print("Downloaded!")
